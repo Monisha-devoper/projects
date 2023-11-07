ba6faa8d2e053c91519083cde6f02b5ccd47c4e0
Monisha-devoper <149255565+Monisha-devoper@users.noreply.github.com>
2023-11-07 12:12:49 +0530

    by clicking addto cart with selector option in amazon.js

--- Amazon/scripts/amazon.js
+++ Amazon/scripts/amazon.js
@@ -27,7 +27,7 @@ products.forEach((product) => {
   </div>
 
   <div class="product-quantity-container">
-    <select>
+    <select class="js-quantity-selector-${product.id}">
       <option selected value="1">1</option>
       <option value="2">2</option>
       <option value="3">3</option>
@@ -68,15 +68,17 @@ button.addEventListener('click',() => {
   cart.forEach((item) =>{
     if(productId === item.productId){
       matchingItem = item;
-    }
-
-  });
+    } });
+    
+    const quantitySelector =  document.querySelector(`.js-quantity-selector-${productId}`);
+    
+    const quantity = Number(quantitySelector.value);
     if(matchingItem){
-      matchingItem.quantity +=1;
+      matchingItem.quantity +=quantity;
     }else{
       cart.push({
         productId:productId,
-        quantity :1 
+        quantity :quantity
       });
     }
 
@@ -84,8 +86,9 @@ button.addEventListener('click',() => {
     cart.forEach((item) =>{
       cartQuantity += item.quantity;
     });
-
+   
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+   
 
 });
 });
