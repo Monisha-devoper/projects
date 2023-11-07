f156f2c487b9acf7a2e2766d8232e174cc68d825
Monisha-devoper <149255565+Monisha-devoper@users.noreply.github.com>
2023-11-07 14:38:24 +0530

    added products and desturcting code in amazon.js

--- Amazon/scripts/amazon.js
+++ Amazon/scripts/amazon.js
@@ -27,7 +27,7 @@ products.forEach((product) => {
   </div>
 
   <div class="product-quantity-container">
-    <select>
+  <select class="js-quantity-selector-${product.id}">
       <option selected value="1">1</option>
       <option value="2">2</option>
       <option value="3">3</option>
@@ -63,7 +63,7 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
 button.addEventListener('click',() => {
   //convert to kebeb case to camel case 'productid to productId'
-  const productId = button.dataset.productId;
+  const {productId} = button.dataset;
   let matchingItem;
   cart.forEach((item) =>{
     if(productId === item.productId){
@@ -71,13 +71,14 @@ button.addEventListener('click',() => {
     }
 
   });
+  const quantitySelector =  document.querySelector(`.js-quantity-selector-${productId}`);
+    
+    const quantity = Number(quantitySelector.value);
     if(matchingItem){
-      matchingItem.quantity +=1;
+      matchingItem.quantity +=quantity;
     }else{
-      cart.push({
-        productId:productId,
-        quantity :1 
-      });
+      cart.push 
+      ({productId,quantity });
     }
 
     let cartQuantity = 0;
