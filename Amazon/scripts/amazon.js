83c643c79431363eed980f693787718c342545b4
Monisha-devoper <149255565+Monisha-devoper@users.noreply.github.com>
2023-11-07 10:08:40 +0530

    added dom in cart

--- Amazon/scripts/amazon.js
+++ Amazon/scripts/amazon.js
@@ -79,7 +79,13 @@ button.addEventListener('click',() => {
         quantity :1 
       });
     }
-  
-  console.log(cart);
+
+    let cartQuantity = 0;
+    cart.forEach((item) =>{
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
 });
 });
